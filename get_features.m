6ea400af6b699bb38170cd99bed34e3e80fccccf
Frank Bagherzadeh <frankbzd@gmail.com>
2020-04-06 23:21:10 -0400

    Removed commented out code and added debug mode

--- get_features.m
+++ get_features.m
@@ -1,9 +1,9 @@
 close all
 clear
 % run('../vlfeat-0.9.20/toolbox/vl_setup')
+debug = false;
 
 pos_imageDir = 'cropped_training_images_faces';
-%pos_imageDir = 'false_negative_validation_images_faces';
 
 pos_imageList = dir(sprintf('%s/*.jpg', pos_imageDir));
 pos_nImages = length(pos_imageList);
@@ -25,7 +25,6 @@ if ~exist(pos_imageDir_v, 'dir')
 end
 
 neg_imageDir = 'cropped_training_images_notfaces';
-%neg_imageDir = 'false_positive_validation_images_faces';
 
 neg_imageList = dir(sprintf('%s/*.jpg', neg_imageDir));
 neg_nImages = length(neg_imageList);
@@ -52,30 +51,36 @@ featSize = 31 * round(36 / cellSize) ^ 2;
 
 pos_feats = zeros(pos_nImages, featSize);
 for i=1:pos_nImages
-	im = im2single(imread(sprintf('%s/%s', pos_imageDir, pos_imageList(i).name)));
-	feat = vl_hog(im, cellSize);
-	pos_feats(i, :) = feat(:);
-%	fprintf('got feat for pos image %d/%d\n', i, pos_nImages);
-%	imhog = vl_hog('render', feat);
-%	subplot(1, 2, 1);
-%	imshow(im);
-%	subplot(1, 2, 2);
-%	imshow(imhog)
-%	pause;
+	if ~debug
+		im = im2single(imread(sprintf('%s/%s', pos_imageDir, pos_imageList(i).name)));
+		feat = vl_hog(im, cellSize);
+		pos_feats(i, :) = feat(:);
+	else
+		fprintf('got feat for pos image %d/%d\n', i, pos_nImages);
+		imhog = vl_hog('render', feat);
+		subplot(1, 2, 1);
+		imshow(im);
+		subplot(1, 2, 2);
+		imshow(imhog)
+		pause;
+	end
 end
 
 neg_feats = zeros(neg_nImages, featSize);
 for i = 1:neg_nImages
-	im = im2single(imread(sprintf('%s/%s', neg_imageDir, neg_imageList(i).name)));
-	feat = vl_hog(im, cellSize);
-	neg_feats(i, :) = feat(:);
-%	fprintf('got feat for neg image %d/%d\n', i, neg_nImages);
-%	imhog = vl_hog('render', feat);
-%	subplot(1, 2, 1);
-%	imshow(im);
-%	subplot(1, 2, 2);
-%	imshow(imhog)
-%	pause;
+	else
+		im = im2single(imread(sprintf('%s/%s', neg_imageDir, neg_imageList(i).name)));
+		feat = vl_hog(im, cellSize);
+		neg_feats(i, :) = feat(:);
+	if ~debug
+		fprintf('got feat for neg image %d/%d\n', i, neg_nImages);
+		imhog = vl_hog('render', feat);
+		subplot(1, 2, 1);
+		imshow(im);
+		subplot(1, 2, 2);
+		imshow(imhog)
+		pause;
+	end
 end
 
 save('pos_neg_feats.mat','pos_feats','neg_feats','pos_nImages','neg_nImages')
